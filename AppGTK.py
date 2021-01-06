9d71ba6b691c52459218e73d073aaac383fcc0f0
Andrey <opaandre@mail.ru>
2021-01-06 17:50:32 +0300

    добавил закрытие программы по коду 666 попытка 2

--- AppGTK.py
+++ AppGTK.py
@@ -391,7 +391,7 @@ class ScannerBox(Gtk.Box):
         elif num == 5:
             self.label.set_markup("<span color='green' size='x-large'> Destroy</span>")
             time.sleep(4)
-            self.close()
+            Gtk.main_quit() 
 
     
     def warningDissapear(self):

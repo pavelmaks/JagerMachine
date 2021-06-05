4a0603fe0a94dfd4f64adbb6414336a3746b7715
Andrey <opaandre@mail.ru>
2021-06-05 16:05:42 +0300

    пробую видео первая форма2

--- testvlc.py
+++ testvlc.py
@@ -26,69 +26,16 @@ class ApplicationWindow(Gtk.Window):
         self.show_all()
 
     def setup_objects_and_events(self):
-        self.playback_button = Gtk.Button()
-        self.stop_button = Gtk.Button()
-
-        self.play_image = Gtk.Image.new_from_icon_name(
-            "gtk-media-play",
-            Gtk.IconSize.MENU
-        )
-        self.pause_image = Gtk.Image.new_from_icon_name(
-            "gtk-media-pause",
-            Gtk.IconSize.MENU
-        )
-        self.stop_image = Gtk.Image.new_from_icon_name(
-            "gtk-media-stop",
-            Gtk.IconSize.MENU
-        )
-
-        self.playback_button.set_image(self.play_image)
-        self.stop_button.set_image(self.stop_image)
-
-        self.playback_button.connect("clicked", self.toggle_player_playback)
-        self.stop_button.connect("clicked", self.stop_player)
 
         self.draw_area = Gtk.DrawingArea()
         self.draw_area.set_size_request(WIDTH, HEIGHT)
 
         self.draw_area.connect("realize", self._realized)
 
-        self.hbox = Gtk.Box(spacing=6)
-        self.hbox.pack_start(self.playback_button, True, True, 0)
-        self.hbox.pack_start(self.stop_button, True, True, 0)
-
         self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.add(self.vbox)
         self.vbox.pack_start(self.draw_area, True, True, 0)
-        self.vbox.pack_start(self.hbox, False, False, 0)
 
-    def stop_player(self, widget, data=None):
-        self.player.stop()
-        self.is_player_active = False
-        self.playback_button.set_image(self.play_image)
-
-    def toggle_player_playback(self, widget, data=None):
-
-        """
-        Handler for Player's Playback Button (Play/Pause).
-        """
-
-        if self.is_player_active == False and self.player_paused == False:
-            self.player.play()
-            self.playback_button.set_image(self.pause_image)
-            self.is_player_active = True
-
-        elif self.is_player_active == True and self.player_paused == True:
-            self.player.play()
-            self.playback_button.set_image(self.pause_image)
-            self.player_paused = False
-
-        elif self.is_player_active == True and self.player_paused == False:
-            self.player.pause()
-            self.playback_button.set_image(self.play_image)
-            self.player_paused = True
-        else:
-            pass
 
     def _realized(self, widget, data=None):
         self.vlcInstance = vlc.Instance("--no-xlib", "--input-repeat=-1")

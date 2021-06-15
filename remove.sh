de2a8819ef7cf6a429d195df7794e7acfc025af6
Raspberry <pavel651@yandex.ru>
2021-06-15 19:51:33 +0300

    Добавил remove update и test

--- remove.sh
+++ remove.sh
@@ -4,6 +4,11 @@ cd /home/pi/Documents
 git clone git@github.com:pavelmaks/JagerMachine.git
 cd JagerMachine
 mv * ../
+mv .* ../
+cd /home/pi/Documents
+rm -rf JagerMachine/*
+rm -rf JagerMachine/.*
 rmdir JagerMachine
+sudo reboot
 
 

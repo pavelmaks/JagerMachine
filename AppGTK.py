b7d99a781aed90eb2b69eb14517fc34839e7e49e
Andrey <opaandre@mail.ru>
2021-06-17 21:17:23 +0300

    чиню баг18

--- AppGTK.py
+++ AppGTK.py
@@ -295,7 +295,7 @@ class ScannerBox(Gtk.Box):#форма сканирования qr кода
         self.image_renderer = Gtk.Image.new_from_pixbuf(self.image)
         self.stack.add_overlay(self.image_renderer)
 
-        ####LABEL
+        '''####LABEL
         labelfixed = Gtk.Fixed()
         self.stack.add_overlay(labelfixed)
 
@@ -310,7 +310,7 @@ class ScannerBox(Gtk.Box):#форма сканирования qr кода
         #self.label.set_valign(15)
         #self.label.set_valign()
         labelbox.add(self.label)
-        labelfixed.put(labelbox, 0, 0)
+        labelfixed.put(labelbox, 0, 0)'''
 
         self.update = False
         ###LABEL END
@@ -356,7 +356,7 @@ class ScannerBox(Gtk.Box):#форма сканирования qr кода
         print('toInstruction')
 
     def setStatusText(self, num):
-        if num == 0: #0 = idle, 1 = invalid, 2 = used
+        '''if num == 0: #0 = idle, 1 = invalid, 2 = used
             self.label.set_markup("<span font='Montserrat' foreground='#ebe6c0' weight='heavy' size='xx-large' letter-spacing ='2300'>     ПОДНЕСИТЕ QR-КОД</span>")
             time.sleep(3)
         elif num == 1:
@@ -379,7 +379,7 @@ class ScannerBox(Gtk.Box):#форма сканирования qr кода
             time.sleep(3)
         elif num > 6:
             self.label.set_markup("<span color='#ffffff' size='x-large'>     Проливка "+str(num)+" секунд</span>")
-            time.sleep(3)
+            time.sleep(3)'''
 
 
 
